8cca69bd01d77a1e4c0dd4f892a0cb6ee25ca71c
Peter Gatrell <peter.gatrell@ntlworld.com>
2020-01-22 17:30:56 +0000

    Added scenes, added basic button script for menu

---
+++ Assets/Scripts/Title Screen/MenuButtons_class.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TState { gameButton, exitButton, noButton }
+
+public class MenuButtons_class : MonoBehaviour
+{
+    TState tState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        buttonPress();
+    }
+
+    void buttonPress()
+    {
+        if (tState == TState.gameButton)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+        }
+
+        if (tState == TState.exitButton)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
