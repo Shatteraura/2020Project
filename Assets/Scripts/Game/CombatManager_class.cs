727970a17e0f843abd3a46e27091565104993bd1
Peter Gatrell <peter.gatrell@ntlworld.com>
2020-02-04 15:36:14 +0000

    Finished combat logic system, Manual only

--- Assets/Scripts/Game/CombatManager_class.cs
+++ Assets/Scripts/Game/CombatManager_class.cs
@@ -7,15 +7,16 @@ public enum computerNodeEnum { noNode, node1, node2, node3, node4 }
 
 public class CombatManager_class : MonoBehaviour
 {
-    playerNodeEnum playerNode;
-    computerNodeEnum computerNode;
+    public playerNodeEnum playerNode;
+    public computerNodeEnum computerNode;
 
     public int playerHealth = 5;
     public int computerHealth = 5;
     public int bonusNumber = 0;
+    public int bonusNumberLocation = 0;
 
-    bool reverseState = false;
-    bool singleLock = false;
+    public bool reverseState = false;
+    public bool singleLock = false;
 
 
     // Start is called before the first frame update
@@ -27,7 +28,27 @@ public class CombatManager_class : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        turnLogic();
+        bonusNumberLogic();
         nodeRelationLogic();
+    }
+
+    //Handles single values and turn over
+    void turnLogic()
+    {
+        if (singleLock == true)
+        {
+            playerNode = playerNodeEnum.noNode;
+            computerNode = computerNodeEnum.noNode;
+            bonusNumberLocation = Random.Range(1, 100);
+            singleLock = false;
+        }
+    }
+
+    //Dictates where the bonus number appears, if at all
+    void bonusNumberLogic()
+    {
+
     }
 
     //The logic that states what beats what
@@ -35,7 +56,8 @@ public class CombatManager_class : MonoBehaviour
     {
         if (reverseState == false)
         {
-            if (playerNode == playerNodeEnum.node1)
+            //Versus Logic
+            if (playerNode == playerNodeEnum.node1 && singleLock == false)
             {
                 if (computerNode == computerNodeEnum.node2 || computerNode == computerNodeEnum.node3)
                 {
@@ -48,9 +70,15 @@ public class CombatManager_class : MonoBehaviour
                     playerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
+
+                if (computerNode == computerNodeEnum.node1 && singleLock == false)
+                {
+                    reverseState = true;
+                    singleLock = true;
+                }
             }
 
-            if (playerNode == playerNodeEnum.node2)
+            if (playerNode == playerNodeEnum.node2 && singleLock == false)
             {
                 if (computerNode == computerNodeEnum.node3)
                 {
@@ -58,31 +86,43 @@ public class CombatManager_class : MonoBehaviour
                     singleLock = true;
                 }
 
-                if (computerNode == computerNodeEnum.node1)
+                if (computerNode == computerNodeEnum.node1 || computerNode == computerNodeEnum.node4)
                 {
                     playerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
+
+                if (computerNode == computerNodeEnum.node2 && singleLock == false)
+                {
+                    reverseState = true;
+                    singleLock = true;
+                }
             }
 
-            if (playerNode == playerNodeEnum.node3)
+            if (playerNode == playerNodeEnum.node3 && singleLock == false)
             {
-                if (computerNode == computerNodeEnum.node4 || computerNode == computerNodeEnum.node2)
+                if (computerNode == computerNodeEnum.node4)
                 {
                     computerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
 
-                if (computerNode == computerNodeEnum.node2)
+                if (computerNode == computerNodeEnum.node2 || computerNode == computerNodeEnum.node1)
                 {
                     playerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
+
+                if (computerNode == computerNodeEnum.node3 && singleLock == false)
+                {
+                    reverseState = true;
+                    singleLock = true;
+                }
             }
 
-            if (playerNode == playerNodeEnum.node4)
+            if (playerNode == playerNodeEnum.node4 && singleLock == false)
             {
-                if (computerNode == computerNodeEnum.node1)
+                if (computerNode == computerNodeEnum.node1 || computerNode == computerNodeEnum.node2)
                 {
                     computerHealth -= 1 - bonusNumber;
                     singleLock = true;
@@ -91,6 +131,12 @@ public class CombatManager_class : MonoBehaviour
                 if (computerNode == computerNodeEnum.node3)
                 {
                     playerHealth -= 1 - bonusNumber;
+                    singleLock = true;
+                }
+
+                if (computerNode == computerNodeEnum.node4 && singleLock == false)
+                {
+                    reverseState = true;
                     singleLock = true;
                 }
             }
@@ -100,7 +146,8 @@ public class CombatManager_class : MonoBehaviour
 
         if (reverseState == true)
         {
-            if (playerNode == playerNodeEnum.node1)
+            //Reversed Versus Logic
+            if (playerNode == playerNodeEnum.node1 && singleLock == false)
             {
                 if (computerNode == computerNodeEnum.node2 || computerNode == computerNodeEnum.node3)
                 {
@@ -113,9 +160,15 @@ public class CombatManager_class : MonoBehaviour
                     computerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
+
+                if (computerNode == computerNodeEnum.node1 && singleLock == false)
+                {
+                    reverseState = false;
+                    singleLock = true;
+                }
             }
 
-            if (playerNode == playerNodeEnum.node2)
+            if (playerNode == playerNodeEnum.node2 && singleLock == false)
             {
                 if (computerNode == computerNodeEnum.node3)
                 {
@@ -123,31 +176,43 @@ public class CombatManager_class : MonoBehaviour
                     singleLock = true;
                 }
 
-                if (computerNode == computerNodeEnum.node1)
+                if (computerNode == computerNodeEnum.node1 || computerNode == computerNodeEnum.node4)
                 {
                     computerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
+
+                if (computerNode == computerNodeEnum.node2 && singleLock == false)
+                {
+                    reverseState = false;
+                    singleLock = true;
+                }
             }
 
-            if (playerNode == playerNodeEnum.node3)
+            if (playerNode == playerNodeEnum.node3 && singleLock == false)
             {
-                if (computerNode == computerNodeEnum.node4 || computerNode == computerNodeEnum.node2)
+                if (computerNode == computerNodeEnum.node4)
                 {
                     playerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
 
-                if (computerNode == computerNodeEnum.node2)
+                if (computerNode == computerNodeEnum.node2 || computerNode == computerNodeEnum.node1)
                 {
                     computerHealth -= 1 - bonusNumber;
                     singleLock = true;
                 }
+
+                if (computerNode == computerNodeEnum.node3 && singleLock == false)
+                {
+                    reverseState = false;
+                    singleLock = true;
+                }
             }
 
-            if (playerNode == playerNodeEnum.node4)
+            if (playerNode == playerNodeEnum.node4 && singleLock == false)
             {
-                if (computerNode == computerNodeEnum.node1)
+                if (computerNode == computerNodeEnum.node1 || computerNode == computerNodeEnum.node2)
                 {
                     playerHealth -= 1 - bonusNumber;
                     singleLock = true;
@@ -156,6 +221,12 @@ public class CombatManager_class : MonoBehaviour
                 if (computerNode == computerNodeEnum.node3)
                 {
                     computerHealth -= 1 - bonusNumber;
+                    singleLock = true;
+                }
+
+                if (computerNode == computerNodeEnum.node4 && singleLock == false)
+                {
+                    reverseState = false;
                     singleLock = true;
                 }
             }
