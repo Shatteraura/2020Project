85c72f63d73be6a6f72db769d7a979e77632ff34
Peter Gatrell <peter.gatrell@ntlworld.com>
2020-02-17 22:13:34 +0000

    Added Button Highlights

--- Assets/Scripts/Game/ButtonScript_class.cs
+++ Assets/Scripts/Game/ButtonScript_class.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ButtonScript_class : MonoBehaviour
 {
     public GameObject combatManagerRef;
+    public SelectBoxScript_class selectRef;
     public int buttonNum;
 
     // Start is called before the first frame update
@@ -24,21 +25,25 @@ public class ButtonScript_class : MonoBehaviour
         if (buttonNum == 1)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 1;
+            selectRef.buttonPos = new Vector3(this.transform.position.x, this.transform.position.y, 1);
         }
 
         if (buttonNum == 2)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 2;
+            selectRef.buttonPos = new Vector3(this.transform.position.x, this.transform.position.y, 1);
         }
 
         if (buttonNum == 3)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 3;
+            selectRef.buttonPos = new Vector3(this.transform.position.x, this.transform.position.y, 1);
         }
 
         if (buttonNum == 4)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 4;
+            selectRef.buttonPos = new Vector3(this.transform.position.x, this.transform.position.y, 1);
         }
     }
 
@@ -47,21 +52,25 @@ public class ButtonScript_class : MonoBehaviour
         if (buttonNum == 1)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 0;
+            selectRef.buttonPos = new Vector3(100, 100, 1);
         }
 
         if (buttonNum == 2)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 0;
+            selectRef.buttonPos = new Vector3(100, 100, 1);
         }
 
         if (buttonNum == 3)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 0;
+            selectRef.buttonPos = new Vector3(100, 100, 1);
         }
 
         if (buttonNum == 4)
         {
             combatManagerRef.GetComponent<CombatManager_class>().currentButton = 0;
+            selectRef.buttonPos = new Vector3(100, 100, 1);
         }
     }
 }
---
+++ Assets/Scripts/Game/SelectBoxScript_class.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectBoxScript_class : MonoBehaviour
+{
+    public Vector3 buttonPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonPos = new Vector3(100, 100, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = buttonPos;
+    }
+}
